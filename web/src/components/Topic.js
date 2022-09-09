1c0c184766eaced8ba3d5fb8eca8f237b92e20ba
eduardo <dudu.falvo@gmail.com>
2022-09-09 00:14:52 -0300

    topic component style

--- web/src/components/Topic.js
+++ web/src/components/Topic.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { TiDeleteOutline } from 'react-icons/ti'
+import { TbEdit } from 'react-icons/tb'
 
-const Topic = () => {
-  return (
-    <div>Topic</div>
-  )
+const Topic = ({ topic }) => {
+    return (
+    <div className='center-row space'>
+        <div className='todo-form topic'>
+            <div className='todo-body middle'>{topic.body}</div>
+            <div className='center-row'>
+                <button className='middle todo-form-btn'><TbEdit /></button>
+                <button className='middle todo-form-btn'><TiDeleteOutline /></button>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default Topic
